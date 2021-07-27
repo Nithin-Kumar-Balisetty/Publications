6fb96b86a960d2a28fb4e2de1c2bcf86db2c7f86
nithinkumarbalisetty98 <nithinkumarbalisetty98@gmail.com>
2021-07-27 12:24:24 +0530

    Google Session vadilation not added

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
-<?php
-    if(isset($_POST)){
-        echo $_POST["smalsmal"];
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homepage</title>
 </head>
 <body>
-<?php
-  $link="../ip/Research/publications.php";
-    echo "<a href='$link'>Redirect to publications page</a>";
-    echo "<script>var temp=0;</script>";
-?>
-<script>
-    <?php
-    echo "console.log('mass ra mowa');";?>
-</script>
-<form action="../ip/index.php" method="POST">
-    <select name="id" id="select">
-        
-    </select>
-    <button type="submit">Click</button>
-</form>
+    
 </body>
 </html>
